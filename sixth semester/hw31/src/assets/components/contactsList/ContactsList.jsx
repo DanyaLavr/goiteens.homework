28ae104493c49f0814bef6a01cbfa70b5cf0ccae
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2025-11-05 09:23:15 +0100

    add hw31

---
+++ sixth semester/hw31/src/assets/components/contactsList/ContactsList.jsx
@@ -0,0 +1,24 @@
+import { useDispatch, useSelector } from "react-redux";
+
+import { removeContact } from "../../../redux/contactsSlice";
+import { getContacts, getFilter } from "../../../redux/selectors";
+export default function Contactslist() {
+  const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+  const filteredContacts = contacts.filter((elem) =>
+    elem.name.toLowerCase().includes(filter.toLowerCase())
+  );
+  return (
+    <ul>
+      {filteredContacts.map(({ id, name, number }) => (
+        <li key={id}>
+          <p>
+            {name} - {number}
+          </p>
+          <button onClick={() => dispatch(removeContact(id))}> Delete</button>
+        </li>
+      ))}
+    </ul>
+  );
+}
